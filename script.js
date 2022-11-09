b4ecc8c84ea9917476695c957dc624ce3ea53394
omarovaassel <omarova.assel@gmail.com>
2022-11-09 13:50:07 +0600

    Second task was modified

--- script.js
+++ script.js
@@ -15,26 +15,29 @@
 //
 // решаем с помощью двух функций, такое мы уже делали в классе function(num) и потом вторая функция, внутри которой берем первую
 
-// function sameNumber(num) {
-//     let array = ("" + num).split("").map(Number); // каждую цифру разбиваю на массив
-//
-//     for(let i = 0; i < array.length; i++) {
-//         return array[i] == array[i + 1]; //это сокращенный if, который вернет мне true или false
-//     }
-// }
-//
-// function sameNumberFinal(arr){
-//     let count = 0;
-//
-//     for(let i = 0; i < arr.length; i++){
-//         if(sameNumber(arr[i])){
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-//
-// console.log( sameNumberFinal([11, 22, 17, 93, 44]) );
+function sameNumber(num) {
+    let array = ("" + num).split(""); // каждую цифру разбиваю на массив
+    let array = num
+    for(let i = 0; i < array.length-1; i++) {
+        if(array[i] !== array[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function sameNumberFinal(arr){
+    let count = 0;
+
+    for(let num of arr){
+        if(sameNumber(num)){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log( sameNumberFinal([11, 223, 17, 93, 44]) );
 
 // 3. Создать функцию, которая принимает значение в секундах, и показывает
 // на консоли значение в минутах и секундах.
